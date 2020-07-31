51cb833534ef25b8933f5a80012a0d0522c1bdd4
Krutarth Trivedi <68107108+krutarth-t18@users.noreply.github.com>
2020-07-31 16:02:58 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,16 @@
+a=int(input("plz enter the value of a"))
+b=int(input("plz enter the value of b"))
+d=input("which task do you want?")
+c=a+b
+if(d is addition):
+        print("addition is",c)
+c=a-b
+if(d is subtraction):
+        print("substration is",c)
+c=a*b
+if(d is multiplication):
+        print("multiplication is",c)
+c=a/b
+if(d is division):
+        print("division is",c)
+    
